e9f43066da6bf1a6d5592289336b9bb46d3c6ed3
timvkim <tim.v.kim@gmail.com>
2023-01-20 20:12:11 +0600

    modified main.go: 2nd task test

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,55 +1,30 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
+
+	"github.com/timvkim/notes/internal/repository"
+	"github.com/timvkim/notes/internal/service"
+	"github.com/timvkim/notes/internal/transport/handlers"
 )
-
-// creating `Note` struct with relevant fields
-type Note struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-}
-
-// `SendNote` implements POST request
-func sendNote(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "application/json")
-	log.Println("creating a new note ...")
-	var note Note
-	err := json.NewDecoder(r.Body).Decode(&note)
-	if err != nil {
-		log.Println("provided json is invalid")
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(err)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(note)
-}
 
 func main() {
 	log.Println("starting server ...")
 
-	http.HandleFunc("/note/save", sendNote)
+	repository := repository.NewRepo()
+	service := service.NewService(repository)
+	handler := handlers.NewHandler(service)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("dislplaying home page ...")
-		io.WriteString(w, "What's the matter?\n")
-	})
+	server := &http.Server{
+		Addr:    ":8090",
+		Handler: handler.InitRouters(),
+	}
 
-	http.HandleFunc("/note/1", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("dislplaying the 1st note ...")
-		io.WriteString(w, "This is the first note\n")
-	})
-
-	err := http.ListenAndServe(":8090", nil)
+	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Printf("error starting server: %s\n", err)
 	}
+
 }
